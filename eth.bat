bfe6c7495233ad9354affe528baccd7bbb3e0c7d
konglingfan <lingfan.kong1@gmail.com>
2018-01-12 00:08:55 -0800

    Create eth.bat

---
+++ eth.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+ethminer.exe --farm-recheck 200 -G -S eu1.ethermine.org:4444 -FS us1.ethermine.org:4444 -O 0x07b3bD27B2212Ef0D387a7cA58DEe28EC60eE39d.eee
